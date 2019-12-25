81544149cca8b6a0ae035745d62f27f7b0d900a6
dkappelle <derk_kappelle@live.nl>
2019-12-26 00:41:07 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+# requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+requirements = []
+
+setup(
+    name="moopy",
+    version="0.0.1",
+    author="Derk Kappelle",
+    author_email="derk_kappelle@live.nl",
+    description="A package for Multi-Objective Optimization tools",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moo4all/MooPy.git",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
